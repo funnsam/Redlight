b9a726904ae71d0a746bfdeb6e657f62dc7d13da
funnsam <funnsam8@gmail.com>
2022-10-24 18:51:49 +0800

    more update

--- redlight.go
+++ redlight.go
@@ -1,38 +1,13 @@
 package redlight
 
 import (
-	"bytes"
-	_ "embed"
-	"fmt"
-	"image/png"
-
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-//go:embed DefaultOff.png
-var rawRedstoneLightOff []byte
-
-//go:embed DefaultOn.png
-var rawRedstoneLightOn []byte
-
 var boolToIntLookup = map[bool]int8{false: 0, true: 1}
 
-var RedstoneLights [2]*ebiten.Image // 0 is off, 1 is on
+var RedstoneLights [2]*ebiten.Image
 var RedstoneLightSize int = 4
-
-func init() {
-	tm1 := bytes.NewReader(rawRedstoneLightOff)
-	tm2 := bytes.NewReader(rawRedstoneLightOn)
-
-	ti1, err := png.Decode(tm1)
-	fmt.Println(err.Error())
-
-	ti2, err := png.Decode(tm2)
-	fmt.Println(err.Error())
-
-	RedstoneLights[0] = ebiten.NewImageFromImage(ti1)
-	RedstoneLights[1] = ebiten.NewImageFromImage(ti2)
-}
 
 func Render(img [][]bool) *ebiten.Image {
 	tempimg := ebiten.NewImage(len(img[0])*RedstoneLightSize, len(img)*RedstoneLightSize)
