a516bca08449567ed1cfd5c9203baebb2521f7cc
funnsam <funnsam8@gmail.com>
2022-10-24 18:53:34 +0800

    more update

--- redlight.go
+++ redlight.go
@@ -12,7 +12,7 @@ var RedstoneLightSize int = 4
 func Render(img [][]bool) *ebiten.Image {
 	tempimg := ebiten.NewImage(len(img[0])*RedstoneLightSize, len(img)*RedstoneLightSize)
 	for y := 0; y < len(img[0]); y++ {
-		var tmp *ebiten.DrawImageOptions
+		tmp := &ebiten.DrawImageOptions{}
 		tmp.GeoM.Translate(0, float64(y*RedstoneLightSize))
 		for x := 0; x < len(img); x++ {
 			tempimg.DrawImage(RedstoneLights[boolToIntLookup[img[y][x]]], tmp)
